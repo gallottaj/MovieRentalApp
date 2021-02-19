2542a879a56dc4faff799bec653f2fe348c67489
joeygallotta <joeygallotta@joeys-mbp>
2021-02-18 17:38:53 -0800

    added User class

---
+++ MovieRentalApp/src/beans/User.java
@@ -0,0 +1,45 @@
+package beans;
+
+
+public class User {
+	private String firstName;
+	private String lastName;
+	private String emailAddress;
+	private String phoneNummber;
+
+	public User() {
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public String getPhoneNummber() {
+		return phoneNummber;
+	}
+
+	public void setPhoneNummber(String phoneNummber) {
+		this.phoneNummber = phoneNummber;
+	}
+
+}
